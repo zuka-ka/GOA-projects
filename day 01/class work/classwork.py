d238070310ec72c7708578a5ba00622a2dcb0974
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-10-05 23:41:57 +0400

    second commit

---
+++ day 01/class work/classwork.py
@@ -0,0 +1,4 @@
+#დავწერე ჩემი სახელი ასაკი და საყვარელი საჭმელი
+print("zuka kakhniashvili")
+print("14 years old")
+print("xinkali and mwvadi")
---
+++ day 2/class work/index .py
@@ -0,0 +1,15 @@
+print("hello")
+print(2025)
+print(67.41)
+
+
+
+name = "zuka"
+age = "14"
+color = "green"
+favfood  = "pizza"
+
+print(name)
+print(age)
+print(color)
+print(favfood)
