35d938eb96929b87da5b30fe3a85a71b07a4ada2
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-10-10 18:05:07 +0400

    third commit

---
+++ day 2/home  work/home work .py
@@ -0,0 +1,35 @@
+#string არის პრინტის სახე რომლის დროსაც დაპრინტული მასალა ბრჭყალებში და ბრჩხილებშია მოქცეული
+#integer არის პრინტის სახე რომლის დროსაც ბრჭყალები არ არის საჭირო და მხოლოდ ბრჩხილები უნდა მხოლოდ რიცხვებზე მუშაობს
+#fload არის პრინტის სახე რომლის დროსაც ბრჭყალები არ არის საჭირო და ბრჩხილებშია მოქცეული წილადი
+
+#string-ის მაგალითებია:
+print("hello world")
+print("my name is zuka")
+print("im 14 years old")
+print("i love my country")
+print("my fav georgia food is khinkali")
+
+#integer-ის მაგალითები:
+print(2025)
+print(14)
+print(200)
+print(67)
+
+#float-ის მაგალითებია:
+print(20.25)
+print(10.20)
+print(67.41)
+print(21.41)
+print(69.67)
+
+name = "zuka"
+age = "14"
+country = "georgia"
+city = "tbilisi"
+food = "khinkali"
+
+print  (name)
+print  (age)
+print  (country)
+print  (city)
+print  (food)
