84015abe9978d076a41aadd101c9c929d0f6bc69
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-10-12 17:17:23 +0400

    fourth commit

---
+++ day 3/class work/class work .py
@@ -0,0 +1,12 @@
+print(2-2)
+print(2+2)
+print(2*2)
+print(2/2)
+print(2%2)
+name = "zuka"
+age = "14"
+color = "green"
+food = "pizza"
+car = "bugati"
+
+print("hello " + "my " + "name " + "is " + (name) + ", " + "i " + "like " + "color " + (color) + ".")
---
+++ day 3/hw/hw3 .py
@@ -0,0 +1,23 @@
+num1 = 3
+num2 = 4
+num3 = 5
+print(num1 + num2 + num3)
+
+length = 20
+width = 10
+print(length*2 + width*2)
+
+print(3-3)
+print(3+3)
+print(3*3)
+print(3/3)
+print(3//3)
+print(3%3)
+
+name = "zuka"
+age = "14"
+color = "green"
+food = "pizza"
+car = "bugati"
+
+print("hello " + "my " + "name " + "is " + (name) + ", " + "i " + "like " + "color " + (color) + ".")
