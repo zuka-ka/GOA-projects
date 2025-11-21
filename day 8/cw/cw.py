a3f59b175ad858b0cc36524ff7a75a9372d2a60a
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-11-21 22:40:52 +0400

    seventh commit

---
+++ day 8/cw/cw.py
@@ -0,0 +1,19 @@
+
+name = input("enter your name")
+print(name)
+
+age = int(input("enter your age"))
+brothers_age = int(input("enter your brothers age"))
+print(age + brothers_age)
+
+age = int(input("enter your age"))
+
+print(type(age))
+
+name = "zuka"
+age = 14
+num = 12.1
+
+print(type(name))
+print(type(age))
+print(type(num))
