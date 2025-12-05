ded8dd5919dc4e160dcdfb154f00feea26c98d42
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-12-06 00:42:36 +0400

    eighth commit

---
+++ day 18/cw/cw.py
@@ -0,0 +1,30 @@
+for i in range(100):
+    print(i)
+
+for b in range(101):
+    print(b)
+
+for a  in range(10,20):
+    print(a)
+
+for c in range(100, 201 , 5):
+    print(c)
+
+for d in range(10 , -1 , -1):
+    print(d)
+
+num = 1
+while num < 10:
+    num += 1
+    print(num)
+
+num1 = 0
+while num1 < 100:
+    num1 += 2
+    print(num1)
+
+num2 = 10 
+while num2 > 0:
+    num2 = num2 -1 
+    print(num2)
+   
---
+++ day 18/hw/hw.py
@@ -0,0 +1,36 @@
+for a  in range(0,39):
+    print(a)
+
+
+    for b  in range(30,40):
+        print(b)
+
+
+for c  in range(12,120):
+    print(c)
+
+
+    name1 = "zuka 14 years old" 
+    for i in range(18):
+        for thing in name1:
+          print(thing)
+          
+
+for i in range(1, 31):
+    print(i, "ვაშლი")
+
+
+age = int(input("Enter your number: "))
+while age < 14:
+    print(age)
+    age +=1
+
+x = 1
+while x < 10:
+    print( "zuka")
+    x += 1
+
+    z = 0
+    while z < 24:
+     print(z)
+     z += 1
