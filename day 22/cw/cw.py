51849d1fb84175ffc03a204f3e7a9d91f106d617
zuka-ka <kakhniashvili.zuka@gmail.com>
2025-12-19 23:07:05 +0400

    tenth

---
+++ day 22/cw/cw.py
@@ -0,0 +1,20 @@
+arr = ["BMW" , "toyota" , "GTR" , "MClaren" , "lexus"]
+print(arr[1], arr[3])
+
+arr = [0, 2, 4, 6, 8, 10]
+print(arr[2], arr[4])
+
+arr = ["dato", "gio", 14, 16, "kakhno"]
+print(arr[4])
+arr[3] = "zuka"
+print(arr)
+
+arr = [5, 8, 7]
+print(arr[0] + arr[2])
+
+#index  -7       -6       -5         -4        -3         -2     -1   
+arr = ["dato", "mari", "jumbera", "manana", "lamzira", "gela", "gio" ]
+arr[-7] = "zuka"
+arr[-3] = "jemala"
+print(arr)
+
